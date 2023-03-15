f0065cc5babfc41a62cd2b5dd7e2f4c0f75603b0
rekesan <redkeans@gmail.com>
2023-03-15 23:27:09 +0800

    update on config files and updated nativewind to nativewind@next

---
+++ metro.config.js
@@ -0,0 +1,4 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+const withNativewind = require("nativewind/metro");
+module.exports = withNativewind(getDefaultConfig(__dirname));
